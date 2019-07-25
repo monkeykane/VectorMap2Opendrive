2cc163a6d8eef31efa090154c4a65b20c53985ab
wukai_51VR <wukai@51hitech.com>
2019-07-25 08:22:02 +0200

    Build Project

---
+++ VectorMap2Opendrive/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorMap2Opendrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
